b0d0544b719364b84602afa735c123c82b018805
Zach Clenaghan <zach.clenaghan@gmail.com>
2016-06-02 15:54:23 +0100

    code complete. bug with the loot showing and being lootable if attackPressed before the new enemy appears

--- RPGOOP/ViewController.swift
+++ RPGOOP/ViewController.swift
@@ -17,17 +17,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerImg: UIImageView!
     @IBOutlet weak var enemyImg: UIImageView!
     
-    @IBAction func onChestTapped(sender: AnyObject) {
-        
-    }
+    var player: Player!
+    var enemy: Enemy!
+    var chestMessage: String?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        
+        playerHpLbl.text = "\(player.hp) HP"
+        
+        generateRandomEnemy()
+        
     }
 
-
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        }
+        else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+        enemyImg.hidden = false
+        
+        enemyHpLbl.text = "\(enemy.hp) HP"
+        
+        printLbl.text = "\(enemy.type) appeared!"
+    }
+    
+    @IBAction func onChestTapped(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(ViewController.generateRandomEnemy), userInfo: nil, repeats: false)
+    }
+    
+    @IBAction func onAttackTapped(sender: UIButton) {
+        
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        }
+        else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
+    }
 
 }
 
