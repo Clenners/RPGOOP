459acb271f1e3195a1adc76c06375ee7f3f6c5b5
Zach Clenaghan <zach.clenaghan@gmail.com>
2016-06-02 12:11:09 +0100

    configured UI. created all necessary Outlets and Actions

--- RPGOOP/ViewController.swift
+++ RPGOOP/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLbl: UILabel!
+    @IBOutlet weak var playerHpLbl: UILabel!
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    @IBOutlet weak var chestBtn: UIButton!
+    @IBOutlet weak var playerImg: UIImageView!
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    @IBAction func onChestTapped(sender: AnyObject) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
